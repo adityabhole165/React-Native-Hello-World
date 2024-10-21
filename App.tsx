9569f1dffae3ad577b523e140bae9d08487aa444
adityabhole165 <adityabhole0304@gmail.com>
2024-10-21 19:16:49 +0530

    Hello World

--- App.tsx
+++ App.tsx
@@ -63,56 +63,25 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <SafeAreaView>
+    {/* <View style={styles.container}> */}
+    <Text style={styles.text}>Hello World</Text>
+        <Text style={styles.text}>Hello World</Text>
+        <Text style={styles.text}>Hello World</Text>
+        <Text style={styles.text}>Hello World</Text>
+        <Text style={styles.text}>Hello World</Text>
+    {/* </View> */}
+ </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  text: {
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    color:'Black',
+    alignContent: 'center',
+    alignItems: 'center',
+  }
 });
 
 export default App;
